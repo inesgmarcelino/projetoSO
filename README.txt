166e00ac6d33ba22acb5e2a8a9d291d2dceff3c1
inesgmarcelino <inesgmarcelino@gmail.com>
2020-11-12 23:04:38 +0000

    initial commit

---
+++ README.txt
@@ -0,0 +1,51 @@
+﻿Sistemas Operativos - Trabalho realizado por:
+GRUPO 21
+Ana Nunes nº 51596 LTI
+Sofia Lourenço nº 54950 LTI
+Inês Marcelino nº 54991 LTI
+
+On file pgrepwc.py, the program is executed with processes.
+On file pgrepwc_threads.py, the program is executed with threads.
+
+NAME
+pgrepwc and pgrepwc_threads - print lines and ocorrences in given files matching the indicated text
+
+DESCRIPTION
+pgrepwc and pgrepwc_threads searches input FILES (or standard input if no files are given) for lines containing a match with given TEXT.
+	
+PYTHON VERSION 3.8
+
+SYNOPSIS
+pgrepwc [-c|-l] text
+pgrepwc [-c|-l] text {files}
+pgrepwc [-c|-l] [-p n] text
+pgrepwc [-c|-l] [-p n] text {files}
+
+
+OPTIONS
+pgrepwc
+	pgrepwc, is required.
+	pgrepwc = "pgrepwc".
+	Defines the beginning of command line.
+	If not given, program won't work properly.
+
+-p n
+	-p n, is optional.
+	n = int. 
+	Defines the number of parallel threads/processes that are used for searching.
+	If this command is not given, only one thread/process will be used.	
+
+[-c|-l]
+	[-c|-l], one of them is required and can't be used at the same time.
+	Doesn't have args.
+	If -c is used, defines the total ocorrences in given files matching the indicated text.
+	If -l is used, defines the total lines in given files matching the indicated text.
+	If one of those is not given, an error occures.
+
+TEXT
+	TEXT, is required.
+	TEXT = str.
+	Defines a string to search from files.
+	If not given, program won't work properly.
+
+
---
+++ pgrepwc_threads.py
@@ -0,0 +1,135 @@
+import os
+import sys
+from multiprocessing import Queue,Value
+from sys import argv
+from sys import stdin
+from threading import Thread
+import time
+import argparse
+import re
+                                                                  
+
+
+###################### THREADING ########################### 
+
+global qFiles
+qFiles = []
+
+def read_files(ind):                         # main function: reads file and prints all the lines with the specific text indicated on command line
+    """
+    Thread read files
+
+    Requires:
+    - ind, an int of the specific thread;
+    Ensures:
+    - prints lines where the word is indicated \
+      and the number of ocurrences of the word in individual files.
+    """
+    while position.value < len(qFiles):      # While the value of position is less that len of list qFiles
+        file =  qFiles[position.value]       # position.value is the position of list qFiles
+        position.value+=1                    # the position of list qFiles is increased
+
+        read_file = open(file, "r")
+        lines = []
+
+        total_words_on_individual_file = 0                  # Variable value with the sum of the word in individual files
+
+##        print("Thread ["+str(ind)+"]")
+        for line in read_file:
+            result = re.sub("[^\w]", " ", line).split()     # Replaces inside the line characters there arent alphanumeric with spaces. [^\w] - not alphanumeric 
+            
+            if text in result:
+##                print("Thread ["+str(ind)+"]",read_file.name, "linha ----> ", line)
+                lines.append(line.split())
+                
+                total.value+=1
+                total_word.value = result.count(text)                           
+                total_words.value += total_word.value                           
+                total_words_on_individual_file += total_word.value
+                time.sleep(0.2)
+        print("\n---------",read_file.name,"---------\n")
+        for words in lines:
+            print(" ".join(words))
+        print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+read_file.name+" : "+str(total_words_on_individual_file))
+
+        read_file.close()
+
+total = Value("i",0)                    
+total_word = Value("i",0)                        
+total_words = Value("i", 0)                      
+position = Value("i",0)                      # Variable value for the position of list qFiles
+
+my_parser = argparse.ArgumentParser()        # Add the arguments so it can automatically create -h and the -c | -l
+
+text = ""
+
+if ("-p" in sys.argv and "-c" in sys.argv) or ("-p" in sys.argv and "-l" in sys.argv):
+    n_processes = sys.argv[sys.argv.index("-p")+1]
+    my_parser.add_argument(dest=text, metavar="text", type=str)
+    if not n_processes.isdigit():
+        raise("Number of processes must be an integer.")
+    else:
+        n_processes = int(n_processes)
+    start = 5
+elif ("-p" not in sys.argv and "-c" in sys.argv) or ("-p" not in sys.argv and "-l" in sys.argv):
+    n_processes = 1
+    start = 3
+    my_parser.add_argument(dest=text, metavar="text", type=str)
+elif ("-p" not in sys.argv and "-c" not in sys.argv) or ("-p" not in sys.argv and "-l" not in sys.argv):  # for argparse add_mutually_exclusive_group error \
+    start = 2                                                                                             # to work even when not -p -c and -l on command line
+elif ("-p" in sys.argv and "-c" not in sys.argv) or ("-p" in sys.argv and "-l" not in sys.argv):          # for argparse add_mutually_exclusive_group error \
+    start = 4                                                                                             # to work even when not -c or -l on command line
+
+
+text = sys.argv[start]                # Index where text is
+files = sys.argv[start+1:]            # Index where files are given, right after the text index
+
+if len(files) == 0:
+    valid = False
+    while not valid:
+        files = input("Indique o(s) ficheiro(s): ").split(" ")   # Stdin files and splits each file between spaces
+        valid = True
+        for file in files:
+            try:
+                test_file=open(file)
+                test_file.close()
+            except:
+                print("File "+str(file)+" is not valid. Please try again")
+                valid = False
+                
+    for file in files:
+        qFiles.append(file)
+
+elif len(files) != 0:
+    for i in files:
+        my_parser.add_argument(dest=i)             # argument of files
+        qFiles.append(i)
+
+group = my_parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-c", action="store_true")      # empty argument
+group.add_argument("-l", action="store_true")      # empty argument
+my_parser.add_argument("-p")
+my_parser.add_argument(dest=text, metavar="text", type=str)
+
+args = my_parser.parse_args()                      # Execute the parse_args() method
+
+threadPool = []
+for i in range(n_processes):                       # Create threads within range of n_processes, and start each thread with target function read_files
+    p = Thread(target=read_files, args=(i,))
+    p.start()
+    threadPool.append(p)
+
+for each in threadPool:
+    each.join()
+
+if "-l" in sys.argv:
+    resume = "\n"+"Total de linhas com a palavra "+"'"+text+"'"+": "+str(total.value)+" linhas"
+    print(resume)
+elif "-c" in sys.argv:
+    resume = "\n"+"Total de ocorrências de "+"'"+text+"'"+" nos ficheiros "+str(files)+": "+str(total_words.value)+" vezes"
+    print(resume)
+
+
+
+
+##################### END THREADING #########################
