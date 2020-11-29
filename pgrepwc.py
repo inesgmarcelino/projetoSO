360f081441e80e9811ecb9eab98874b8d602a57a
anaaaaaaaaa <38542351+anaaaaaaaaa@users.noreply.github.com>
2020-11-29 02:37:50 +0000

    Tudo feito, exceto SIGINT
    Já consegui fazer a divisao de linhas do ficheiro por processos. Porém, se conseguirem fazer de outra forma era aconselhado dado que o que eu fiz foi, por cada processo, ele le as linhas de um ficheiro paralelamente. Se conseguissem fazer para que cada processo ler apenas aquele ficheiro que esta repetido e de x a x linhas, era otimo.
    O SIGINT ainda nao consegui fazer, mas se fizessem isso tambem era otimo.
    Em termos de organização de codigo/comentarios ainda nao estao feitos

--- pgrepwc.py
+++ pgrepwc.py
@@ -1,195 +1,54 @@
 import os
-import sys
-from multiprocessing import Queue,Value,Semaphore,Manager,Array
+##import sys
+from multiprocessing import Queue,Value,Pool
 from multiprocessing import Process
 import threading
 from sys import argv
-from sys import stdin
 import re
 import time
 import signal
 import pickle
 import datetime
-from ctypes import c_char_p
 
 
-##################### VARIAVEIS GLOBAIS ###############################
+
+##################### GLOBAL VARIABLES ############################
 
 global qFiles
-qFiles = Queue()  # Queue qFiles with files indicated on command line
+qFiles = Queue() # Queue qFiles with files indicated on command line
 
 global duration
 duration = time.time()
 
-global l
-l = []
+global lines
+lines = Queue()
+
+global files_update
+files_update = []
+
+ProcessPool = []
+
+
+##################### VALUES VARIABLES ############################
 
 total = Value("i", 0)                      # variable value that is going to be the position of list qFiles
 total_word = Value("i", 0)                 # Variable value with the count of the text in a line
 total_words = Value("i", 0)                # Variable value with the sum of all the total_word value
+total_words_file = Value("i",0)
 processed = Value("i", 1)                  # variable value with the total number of processed files
-##total_words_individual_files = Array(c_char_p, [])
-total_words_individual_files = Value("i", 0)
+##counter = Value("i", 0)
+position = Value("i", 0)
+total_words_on_individual_file = Value("i", 0)
+
+
+##################### LOCKS VARIABLES #############################
 threadLock = threading.Lock()
 mutex = threading.Lock()
 mutex_two = threading.Lock()
 
 
+########################### BEGIN MAIN PROCESS ####################
 
-##################### FINAL VARIAVEIS GLOBAIS ###############################
-
-
-########################### SIGNALS #######################################
-def ctrl (sig, NULL): 
-    """
-    ESTÁ ERRADO. ERROS NO VALOR DE TOTAL, NAO TERMINA QUANDO OS PROCESSOS TERMINAM O PROCESSAMENTO DOS SEUS PRIMEIROS FICHEIROS
-    """
-    if "-l" in sys.argv:
-        for i in range(len(l)):
-            print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+str(l[i])+" : "+str(total_words_individual_files.value)+"\n"+"Total de linhas com a palavra "+"'"+text+"'"+" nos ficheiros processados: "+str(total.value)+" linhas")
-            sys.exit()
-    if "-c" in sys.argv:
-        for i in range(len(l)):
-            print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+str(l[i])+" : "+str(total_words_individual_files.value)+"\n"+"Total de ocorrências de "+"'"+text+"'"+" nos ficheiros processados: "+str(total_words.value)+" vezes")
-            sys.exit()
-    
-
-def print_state(sig, NULL): # function triggered by ALARM SIGNAL
-    """
-    FEITO
-    """
-    print("\nNúmero de ficheiros processados: "+str(processed.value))
-    if "-l" in sys.argv:
-        print("\nNúmero corrente de linhas onde a palavra foi encontrada: "+str(total.value))
-    elif "-c" in sys.argv:
-        print("\nNúmero corrente de ocorrências onde a palavra foi encontrada: "+str(total_words.value))
-    currentDuration = (time.time() - duration)*1000 #calculates current program execution time
-    print("Tempo de execução corrente: "+str(currentDuration)+" microseconds")
-
-
-########################### FINAL SIGNALS #######################################
-
-
-########################### METODOS #######################################
-def processes_bigger_files(ind):
-    """
-    FALTA DEFINIR SE PROCESSOS MAIOR QUE FICHEIROS, DIVIDIR OS FICHEIROS, O PROBLEMA ESTA PROVAVELMENTE NO FILE = QfILES.GET()
-    """
-    threadLock.acquire()
-    file = qFiles.get()
-    l.append(file)
-    threadLock.release()
-
-    read_file = open(file, "r")
-    lines = []
-    search_time = time.time()                               # initial starting time for file processing
-    total_lines = 0
-    total_words_on_individual_file = 0
-
-    for line in read_file:
-        result = re.sub("[^\w]", " ", line).split()         # replaces inside the line characters there arent alphanumeric with spaces. [^\w] - not alphanumeric
-        if text in result:
-            mutex.acquire()
-            lines.append(line.strip())
-            total_lines += 1
-            total.value += 1
-            total_word.value = result.count(text)
-            total_words.value += total_word.value           
-            total_words_on_individual_file += total_word.value
-            mutex.release()
-
-    search_time = duration - search_time
-    time.sleep(2)
-
-    print("\n---------",read_file.name,"---------\n")
-    for words in lines:
-        print(" ".join(words)+"\n")
-        
-    print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+read_file.name+" : "+str(total_words_on_individual_file))
-    read_file.close()
-
-
-def processes_less_equal_files(ind):
-    """
-    FEITO
-    """
-    threadLock.acquire()
-    file = qFiles.get()
-    l.append(file)
-    threadLock.release()
-
-    read_file = open(file, "r")
-    lines = []
-    search_time = time.time()                               # initial starting time for file processing
-    total_lines = 0
-    total_words_on_individual_file = 0
-
-    for line in read_file:
-        result = re.sub("[^\w]", " ", line).split()         # replaces inside the line characters there arent alphanumeric with spaces. [^\w] - not alphanumeric
-        if text in result:
-            mutex.acquire()
-            lines.append(line.strip())
-            total_lines += 1
-            total.value += 1
-            total_word.value = result.count(text)
-            total_words.value += total_word.value           
-            total_words_on_individual_file += total_word.value
-            mutex.release()
-
-    search_time = duration - search_time
-    time.sleep(2)
-    print("\n---------",read_file.name,"---------\n")
-    for words in lines:
-        print(" ".join(words)+"\n")
-        
-    print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+read_file.name+" : "+str(total_words_on_individual_file))
-    read_file.close()
-
-def read_files(ind):   # metodo main               
-    """
-    Processes read files
-
-    Requires:
-    - ind, an int of the specific process;
-    Ensures:
-    - prints lines where the word is indicated \
-      and the number of ocurrences of the word in individual files.
-    """
-    
-    while qFiles.empty() is False:
-
-        if n_processes > len(files):
-            processes_bigger_files(ind)
-
-        else:
-            processes_less_equal_files(ind)
-
-        if "-f" in sys.argv:
-            with open(log_file, 'ab') as handle:
-                if "-c" in sys.argv:
-                    mode = "-c"
-                    pickle.dump([os.getpid(),read_file.name,search_time,os.path.getsize(read_file.name), total_words_on_individual_file, mode], handle, protocol=pickle.HIGHEST_PROTOCOL) # writes in the log file
-                elif "-l" in sys.argv:
-                    mode = "-l"
-                    pickle.dump([os.getpid(),read_file.name,search_time,os.path.getsize(read_file.name), total_lines, mode], handle, protocol=pickle.HIGHEST_PROTOCOL) # writes in the log file
-                    
-        mutex_two.acquire()
-        processed.value+=1
-        mutex_two.release()
-
-def final_print():
-    if "-l" in sys.argv:
-        resume = "\n"+"Total de linhas com a palavra "+"'"+text+"'"+": "+str(total.value)+" linhas"
-        print(resume)
-    elif "-c" in sys.argv:
-        resume = "\n"+"Total de ocorrências de "+"'"+text+"'"+" nos ficheiros "+str(files)+": "+str(total_words.value)+" vezes"
-        print(resume)
-
-########################### FINAL METODOS #######################################
-        
-signal.signal(signal.SIGINT, ctrl)      # SIGINT signal that triggers function ctrl
-
-########################### INICIO DO PROGRAMA #######################################
 if ("-c" in sys.argv or "-l" in sys.argv):
     if "-p" in sys.argv:
         n_processes = sys.argv[sys.argv.index("-p")+1]
@@ -215,20 +74,20 @@ if ("-c" in sys.argv or "-l" in sys.argv):
 
     if "-f" in sys.argv:
         log_file = sys.argv[sys.argv.index("-f")+1]
-        if not ".bin" in log_file:
+        if not ".bin" in log_file:                       # In case the user forgets to put the name file or the .bin on file
             start+=1
-            valid = False
+            valid = True
             while not valid:
                 log_file = input("Deseja indicar o nome do ficheiro bin (Se 'n' o ficheiro será Default.bin) (y/n): ")
                 if log_file == "y":
-                    log_file = input("Indique o nome do ficheiro bin (file.bin): ")
+                    log_file = input("Indique o nome do ficheiro bin (name_file.bin): ")
                     if not ".bin" in log_file:
-                        valid = False
-                    else:
                         valid = True
+                    else:
+                        valid = False
                 elif log_file == "n":
                     log_file = "Default.bin"
-                    valid = True
+                    valid = False
         elif ".bin" in log_file:
             log_file = sys.argv[sys.argv.index("-f")+1]
             start+=2
@@ -262,26 +121,182 @@ if ("-c" in sys.argv or "-l" in sys.argv):
         with open(log_file, 'ab') as handle:
             pickle.dump(current_time, handle, -1)    # writes in the log file the time in which the program has started
 
-    ProcessPool = []
-    for i in range(n_processes):
-        p = Process(target=read_files, args=(i,))
-        p.start()
-        ProcessPool.append(p)
+    defining_numberP_to_Files_or_lines()
 
-    time.sleep(0.5)
-    
+    signal.signal(signal.SIGINT, ctrl)      # SIGINT signal that triggers function ctrl  ??
+
     for each in ProcessPool:
         each.join()
     
     duration = time.time() - duration  # calculates final duration of the whole program
+
     if "-f" in sys.argv:
         with open(log_file, 'ab') as handle:
             pickle.dump(time.strftime("%H:%M:%S:%ms", time.gmtime(duration)), handle, -1) # writes in the log file that same duration as seen above
 
-    final_print()
-    
+    total()
+
 else:
     print("Error: -c or -l must be included on the command line.")                  
 
 
-########################### FINAL DO PROGRAMA #######################################
+########################### END MAIN PROCESS ###########################
+
+
+########################### DEFINING METHODS TO USE ####################
+def defining_numberP_to_Files_or_lines():
+
+    if n_processes > len(files):
+        for file in files:
+            with open(file) as source_file:
+                original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+                p = Pool(processes = n_processes)
+                signal.signal(signal.SIGINT, original_sigint_handler)
+                p.map(processes_bigger_files, source_file)
+                p.close()
+
+            p_main = Process(target=processes_bigger_files_print, args=(i, ))
+            p_main.start()
+            ProcessPool.append(p)
+            ProcessPool.append(p_main)
+
+    elif n_processes <= len(files):
+        for i in range(n_processes):
+            p = Process(target=processes_less_equal_files, args=(i, ))
+            p.start()
+            ProcessPool.append(p)
+
+###################### PROCESS PER LINE METHOD ######################
+def processes_bigger_files(line):
+    """
+    FEITO
+    """
+    
+    files_update.append(source_file.name)
+    search_time = time.time()                               # initial starting time for file processing
+    result = re.sub("[^\w]", " ", line).split()         # replaces inside the line characters there arent alphanumeric with spaces. [^\w] - not alphanumeric
+    total_lines = 0
+
+    if text in result:
+        mutex.acquire()
+        lines.put(line.strip())
+        total_lines += 1
+        total.value += 1
+        total_word.value = result.count(text)
+        total_words.value += total_word.value           
+        total_words_on_individual_file.value += total_word.value
+        total_words_file.value = total_words_on_individual_file.value
+        mutex.release()
+
+    search_time = duration - search_time
+    time.sleep(0.2)
+
+    if "-f" in sys.argv:
+        with open(log_file, 'ab') as handle:
+            if "-c" in sys.argv:
+                mode = "-c"
+                pickle.dump([os.getpid(),read_file.name,search_time,os.path.getsize(read_file.name), total_words_on_individual_file.value, mode], handle, protocol=pickle.HIGHEST_PROTOCOL) # writes in the log file
+            elif "-l" in sys.argv:
+                mode = "-l"
+                pickle.dump([os.getpid(),read_file.name,search_time,os.path.getsize(read_file.name), total_lines, mode], handle, protocol=pickle.HIGHEST_PROTOCOL)  
+
+###################### PROCESS PER FILE METHOD #####################
+def processes_less_equal_files(line):               
+    """
+    Processes read files
+
+    Requires:
+    - ind, an int of the specific process;
+    Ensures:
+    - prints lines where the word is indicated \
+      and the number of ocurrences of the word in individual files.
+    """
+
+    while qFiles.empty() is False:
+        threadLock.acquire()
+        file = qFiles.get()
+        threadLock.release()
+        read_file = open(file, "r")
+        files_update.append(read_file)
+        total_words_on_individual_file = 0
+        total_lines = 0
+        search_time = time.time()                               # initial starting time for file processing
+
+        for line in read_file:
+            result = re.sub("[^\w]", " ", line).split()         # replaces inside the line characters there arent alphanumeric with spaces. [^\w] - not alphanumeric
+            if text in result:
+                mutex.acquire()
+                lines.put(line.strip())
+                total_lines += 1
+                total.value += 1
+                total_word.value = result.count(text)
+                total_words.value += total_word.value           
+                total_words_on_individual_file += total_word.value
+                total_words_file.value = total_words_on_individual_file
+                time.sleep(0.3)
+                mutex.release()
+                
+        search_time = duration - search_time
+
+        if "-f" in sys.argv:
+            with open(log_file, 'ab') as handle:
+                if "-c" in sys.argv:
+                    mode = "-c"
+                    pickle.dump([os.getpid(),read_file.name,search_time,os.path.getsize(read_file.name), total_words_on_individual_file, mode], handle, protocol=pickle.HIGHEST_PROTOCOL) # writes in the log file
+                elif "-l" in sys.argv:
+                    mode = "-l"
+                    pickle.dump([os.getpid(),read_file.name,search_time,os.path.getsize(read_file.name), total_lines, mode], handle, protocol=pickle.HIGHEST_PROTOCOL) # writes in the log file
+
+        processes_bigger_files_print(read_file.name)
+
+        read_file.close()
+
+###################### OUTPUT PER FILE ########################
+def processes_bigger_files_print(source_file):
+    """
+    """
+    print("\n---------",source_file,"---------\n")
+    while lines.empty() is False:
+        print(lines.get()+"\n")
+        
+    print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+source_file+" : "+str(total_words_file.value))
+
+    mutex_two.acquire()
+    processed.value+=1
+    total_words_on_individual_file.value = 0
+##    counter.value+=1
+    mutex_two.release()
+
+###################### TOTAL OUTPUT ###########################
+def total():
+    if "-l" in sys.argv:
+        resume = "\n"+"Total de linhas com a palavra "+"'"+text+"'"+" nos ficheiros "+str(", ".join(files)+" ")+": "+str(total.value)+" linhas"
+        print(resume)
+    elif "-c" in sys.argv:
+        resume = "\n"+"Total de ocorrências de "+"'"+text+"'"+" nos ficheiros "+str(", ".join(files)+" ")+": "+str(total_words.value)+" vezes"
+        print(resume)
+
+########################### SIGNALS ############################
+def ctrl (sig, NULL): 
+    """
+    NAO TERMINA QUANDO OS PROCESSOS TERMINAM O PROCESSAMENTO DOS SEUS PRIMEIROS FICHEIROS
+##    """
+    for i in range(len(files_update)):
+        print("\n---------",str(files_update[i].name),"---------\n")
+        for words in lines:
+            print(" ".join(words)+"\n")
+                        
+        print("\nocorrências de "+"'"+text+"'"+" no ficheiro "+str(files_update[i].name)+" : "+str(total_words_file.value))
+        sys.exit()
+
+def print_state(sig, NULL): # function triggered by ALARM SIGNAL
+    """
+    FEITO
+    """
+    print("\nNúmero de ficheiros processados: "+str(processed.value))
+    if "-l" in sys.argv:
+        print("\nNúmero corrente de linhas onde a palavra foi encontrada: "+str(total.value))
+    elif "-c" in sys.argv:
+        print("\nNúmero corrente de ocorrências onde a palavra foi encontrada: "+str(total_words.value))
+    currentDuration = (time.time() - duration)*1000 #calculates current program execution time
+    print("Tempo de execução corrente: "+str(currentDuration)+" microseconds")
